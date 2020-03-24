108b105572000cdb8518ab9e1c930b008d70cef1
davide-giacomini <dadegeco98@gmail.com>
2020-03-24 17:23:04 +0100

    Added test source.

---
+++ src/test/java/LevelTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+public class LevelTest {
+    
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+    
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
