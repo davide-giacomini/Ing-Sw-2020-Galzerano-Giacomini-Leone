5497f1f58b152725f1890ce90913de31b46f7443
davide-giacomini <dadegeco98@gmail.com>
2020-03-24 16:57:18 +0100

    First commit.

---
+++ src/main/java/Level.java
@@ -0,0 +1,14 @@
+public enum Level {
+    
+    GROUND (0),
+    LEVEL1 (1),
+    LEVEL2 (2),
+    LEVEL3 (3),
+    DOOM (4);
+    
+    private final int level; // in meters
+    Level(int level) {
+        this.level = level;
+    }
+    
+}
