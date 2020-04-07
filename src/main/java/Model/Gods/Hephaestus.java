4416d494a5e60134d4ab631774323b42d07ac295
arigalzi <arianna.galzerano@mail.polimi.it>
2020-04-07 13:08:54 +0200

    Added Hephaestus Class and added methods of CLI

--- src/main/java/Model/Gods/Hephaestus.java
+++ src/main/java/Model/Gods/Hephaestus.java
@@ -10,45 +10,49 @@ import Model.Player;
 import Model.Slot;
 import Model.Worker;
 
+
 /**
- * {@link Player}'s {@link Worker} may build one additional time, but not on the same space.
+ * {@link Player}'s {@link Worker} may build one additional time, but only on the same space.
  */
-public class Demeter extends God {
-    // the slot where Demeter builds the first time
-    private Slot previousSlot;
-    
-    public Demeter(Player player, String name) {
-        super(player, name);
+public class Hephaestus extends God {
+
+    // the slot where Hephaestus builds the first time
+    // and where it can build for a second time
+    private Slot doubleBuildSlot;
+
+    public Hephaestus(Player player, String name){
+        super (player, name);
         MAX_BUILDINGS = 2;
         MIN_BUILDINGS = 1;
         MAX_MOVEMENTS = 1;
         MIN_MOVEMENTS = 1;
         canBuildDome = false;
         canUseBothWorkers = false;
-    }
-    
+}
+
     @Override
     public boolean move(Direction direction, Worker worker)  throws SlotOccupiedException, NotReachableLevelException, IndexOutOfBoundsException, InvalidDirectionException, WrongBuildOrMoveException {
+
         return worker.move(direction);
     }
-    
+
     @Override
-    public void build(Direction direction, Worker worker)
-            throws IndexOutOfBoundsException, SlotOccupiedException, InvalidDirectionException, WrongBuildOrMoveException {
-    
+    public void build(Direction direction, Worker worker) throws IndexOutOfBoundsException, SlotOccupiedException, InvalidDirectionException, WrongBuildOrMoveException {
+
         if (player.getTurn().getNumberOfMovements() == 0) throw new WrongBuildOrMoveException();
-    
+
         if (player.getTurn().getNumberOfBuildings() == 0)
-            previousSlot = Board.getNearbySlot(direction, worker.getSlot());
-        else if (Board.getNearbySlot(direction, worker.getSlot()).equals(previousSlot))
+            doubleBuildSlot = Board.getNearbySlot(direction, worker.getSlot());
+        else if (!Board.getNearbySlot(direction, worker.getSlot()).equals(doubleBuildSlot))
             throw new WrongBuildOrMoveException();
-        
+
         worker.build(direction);
-    
+
     }
-    
+
     @Override
     public void resetParameters() {
-        previousSlot = null;
+        doubleBuildSlot = null;
     }
+
 }
