9df310f1cc613d0fa66c02414dfea08ebbe9b03a
arigalzi <arianna.galzerano@mail.polimi.it>
2020-05-16 16:55:31 +0200

    added Mockito
    -started GameControllerTest
    -tried first mock classes

--- src/test/java/it/polimi/ingsw/PSP47/Controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/PSP47/Controller/GameControllerTest.java
@@ -4,7 +4,13 @@ import it.polimi.ingsw.PSP47.Enumerations.Color;
 import it.polimi.ingsw.PSP47.Network.Server.ClientHandler;
 import it.polimi.ingsw.PSP47.Network.Server.VirtualView;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.*;
 
 import java.net.Socket;
 import java.util.HashMap;
@@ -13,22 +19,36 @@ import static org.junit.Assert.assertEquals;
 
 public class GameControllerTest {
     private GameController controller;
-    private HashMap<String, Color> mapUserColor;
-    private HashMap<String, VirtualView> mapUserVirtualView;
+    private HashMap<String, Color> mapUserColor = new HashMap<>();
+    private HashMap<String, VirtualView> mapUserVirtualView = new HashMap<>();
 
 
+    @Mock
+    ClientHandler ariannaClientMock;
+
+    @Mock
+    ClientHandler monicaClientMock ;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Before
     public void setUp() {
         int numberOfPlayers = 2;
 
-        ClientHandler ariannaClient = new ClientHandler(new Socket(), false);
-        ClientHandler monicaClient = new ClientHandler(new Socket(), false);
         mapUserColor.put("Arianna", Color.BLUE);
-        mapUserVirtualView.put("Arianna", new VirtualView("Arianna", Color.BLUE, ariannaClient));
+        VirtualView v1 = new VirtualView("Arianna", Color.BLUE, ariannaClientMock);
+        mapUserVirtualView.put("Arianna",  v1);
         mapUserColor.put("Monica", Color.YELLOW);
-        mapUserVirtualView.put("Monica", new VirtualView("Monica", Color.YELLOW, monicaClient));
+        mapUserVirtualView.put("Monica", new VirtualView("Monica", Color.YELLOW, monicaClientMock));
         controller = new GameController(numberOfPlayers, mapUserColor, mapUserVirtualView);
     }
+
+    @Test
+    public void startController(){
+        assertEquals(controller.getNumberOfPlayers(),2);
+    }
+
+
 
 }
